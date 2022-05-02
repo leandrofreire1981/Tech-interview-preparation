bd84e6b69ab579e29d3deee3e7106f9d3a64f387
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-05-02 16:43:18 -0300

    tests added

--- Tests/11-ClockMinuteAdder.spec.js
+++ Tests/11-ClockMinuteAdder.spec.js
@@ -11,7 +11,10 @@ xdescribe('ClockMinuteAdder', function () {
   it("Should return '01:45' if add 100 minutes to '12:05'", () => {
     expect(clockMinuteAdder('12:05', 100)).to.equal('01:45')
   })
-  it("Should return '12:00' if add 90 minutes to '10:30'", () => {
-    expect(clockMinuteAdder('10:30', 90)).to.equal('12:00')
+  it("Should return '08:00' if add 90 minutes to '06:30'", () => {
+    expect(clockMinuteAdder('06:30', 90)).to.equal('08:00')
+  })
+  it("Should return '12:05' if add 1440 minutes to '12:50'", () => {
+    expect(clockMinuteAdder('12:05', 1440)).to.equal('12:05')
   })
 })
