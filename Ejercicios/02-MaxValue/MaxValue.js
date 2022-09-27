e24d7196fcf4b7c37eb8d42a76478ff91437d2f4
Leandro <freire.leandro@hotmail.com>
2022-09-27 12:47:38 -0300

    max Value

--- Ejercicios/02-MaxValue/MaxValue.js
+++ Ejercicios/02-MaxValue/MaxValue.js
@@ -1,6 +1,10 @@
 function MaxValue (shares) {
   // Your code here:
-
+  const min = Math.min(...shares)
+  const i = shares.indexOf(min)
+  const max = Math.max(...shares.slice(i))
+  return max - min
 }
 
+//console.log(MaxValue([4, 3, 2, 5, 11]))
 module.exports = MaxValue
